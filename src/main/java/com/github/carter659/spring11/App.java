27e6f3e97c4acc5133dd201a4887dc3deefca260
carter659 <24084182@qq.com>
2017-01-08 23:26:54 +0800

    ok

---
+++ src/main/java/com/github/carter659/spring11/App.java
@@ -0,0 +1,17 @@
+package com.github.carter659.spring11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+}
